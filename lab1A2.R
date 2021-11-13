cf9acd5fcffdd815f6f0da6821acf01925459330
Xiaolin-Jiang <90145823+Xiaolin-Jiang@users.noreply.github.com>
2021-11-13 12:28:46 +0100

    Not solution till 2.2

--- lab1A2.R
+++ lab1A2.R
@@ -15,6 +15,11 @@ test=df_scale[-id,]
 
 # 2.2
 fit1=lm(motor_UPDRS~ 0 + Jitter...+Jitter.Abs.+Jitter.RAP+Jitter.PPQ5+Jitter.DDP+Shimmer+Shimmer.dB.+Shimmer.APQ3+Shimmer.APQ5+Shimmer.APQ11+Shimmer.DDA+NHR+HNR+RPDE+DFA+PPE, data=train)
-summary(fit1)
-# fit2=predict(fit1, test)
-# summary(fit2)
+MSE_train = mean(fit1$residuals^2)   # 512.82
+Test_fitted=predict(fit1, test)
+MSE_test = mean((test$motor_UPDRS-Test_fitted)^2) #513.95
+#summary(fit1)
+# From the result of the summary of the training model, the significant variables are 
+# DFA, NHR, HNR, and PPE. 
+
+
